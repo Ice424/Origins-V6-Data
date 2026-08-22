f82a12e9aa2291c45ba140413ab93f3d9864bc32
Ice424 <e4elliotd@gmail.com>
2026-08-22 17:12:44 +0100

    i dont even know

--- kubejs/startup_scripts/flowers.js
+++ kubejs/startup_scripts/flowers.js
@@ -1,22 +1,4 @@
-const aqua_flower = player => {
-    player.tell("aqua")
-}
-const purple_flower = player => {
-    player.tell("purple")
-}
-const orange_flower = player => {
-    player.tell("orange")
-}
-const green_flower = player => {
-    player.tell("green")
-}
-const yellow_flower = player => {
-    player.tell("yellow ")
-}
-const blue_flower = player => {
-    player.tell("blue")
 
-}
 
 
 StartupEvents.registry('item', event => {
@@ -42,7 +24,7 @@ StartupEvents.registry('item', event => {
             itemstack.shrink(1)
 
             if (entity.player) {
-                aqua_flower(entity)
+                global.aqua_flower(entity)
             }
 
             return itemstack
@@ -70,7 +52,7 @@ StartupEvents.registry('item', event => {
             itemstack.shrink(1)
 
             if (entity.player) {
-                purple_flower(entity)
+                global.purple_flower(entity)
             }
 
             return itemstack
@@ -98,7 +80,7 @@ StartupEvents.registry('item', event => {
             itemstack.shrink(1)
 
             if (entity.player) {
-                orange_flower(entity)
+                global.orange_flower(entity)
             }
 
             return itemstack
@@ -126,7 +108,7 @@ StartupEvents.registry('item', event => {
             itemstack.shrink(1)
 
             if (entity.player) {
-                green_flower(entity)
+                global.green_flower(entity)
             }
 
             return itemstack
@@ -154,7 +136,7 @@ StartupEvents.registry('item', event => {
             itemstack.shrink(1)
 
             if (entity.player) {
-                yellow_flower(entity)
+                global.yellow_flower(entity)
             }
 
             return itemstack
@@ -182,7 +164,34 @@ StartupEvents.registry('item', event => {
             itemstack.shrink(1)
 
             if (entity.player) {
-                blue_flower(entity)
+                global.blue_flower(entity)
+            }
+
+            return itemstack
+        })
+    event.create("ice:omega_flower").maxStackSize(16)
+        .useAnimation("bow")
+        .useDuration(itemstack => 64)
+        .use((level, player, hand) => {
+            if (level.isClientSide()) {
+                return false
+            }
+
+            const nbt = player.nbt
+            const origin =
+                nbt['neoforge:attachments']?.['neoorigins:origin_data']?.origins?.['neoorigins:origin']
+            return origin === 'ice:flowery'
+        })
+
+        .finishUsing((itemstack, level, entity) => {
+            if (level.isClientSide()) {
+                return itemstack
+            }
+
+            itemstack.shrink(1)
+
+            if (entity.player) {
+                global.omega_flower(entity)
             }
 
             return itemstack
---
+++ kubejs/startup_scripts/flowers_powers.js
@@ -0,0 +1,23 @@
+global.aqua_flower = player => {
+    player.tell("aqua")
+}
+global.purple_flower = player => {
+    player.tell("purple")
+}
+global.orange_flower = player => {
+    player.tell("orange")
+}
+global.green_flower = player => {
+    player.tell("green")
+}
+global.yellow_flower = player => {
+    player.tell("yellow ")
+}
+global.blue_flower = player => {
+    player.tell("blue")
+
+}
+global.omega_flower = player => {
+    player.tell("FLOWERY")
+
+}
